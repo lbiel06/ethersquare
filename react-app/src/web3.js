ceb069ed96f217fb37c793d65b0d3fc25251fef4
= <=>
2024-04-30 14:44:40 +0200

    Added RPC provider

--- react-app/src/web3.js
+++ react-app/src/web3.js
@@ -1,5 +1,7 @@
 import Web3 from "web3"
 
-const web3 = new Web3(window.ethereum)
+const RPC_PROVIDER = "https://ethereum-sepolia-rpc.publicnode.com"
+
+const web3 = window.etherum ? new Web3(window.etherum) : new Web3(RPC_PROVIDER)
 
 export default web3
