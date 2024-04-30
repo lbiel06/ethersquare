213063c6147a331d11a5d10d0364513a1186aac8
= <=>
2024-04-30 14:49:05 +0200

    Fix

--- react-app/src/web3.js
+++ react-app/src/web3.js
@@ -1,7 +1,5 @@
 import Web3 from "web3"
 
-const RPC_PROVIDER = "https://ethereum-sepolia-rpc.publicnode.com"
-
-const web3 = window.etherum ? new Web3(window.etherum) : new Web3(RPC_PROVIDER)
+const web3 = new Web3(window.ethereum)
 
 export default web3
