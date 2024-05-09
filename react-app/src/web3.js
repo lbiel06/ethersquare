45fd405a939154aedbb4f260fe62c7ac2e7c2b85
= <=>
2024-05-09 15:10:54 +0200

    Added public node support

--- react-app/src/web3.js
+++ react-app/src/web3.js
@@ -1,5 +1,12 @@
 import Web3 from "web3"
 
-const web3 = new Web3(window.ethereum)
+let web3 = null
+
+if (window.ethereum) {
+  web3 = new Web3(window.ethereum)
+} else {
+  web3 = new Web3("https://ethereum-sepolia-rpc.publicnode.com")
+  console.log("using public node")
+}
 
 export default web3
